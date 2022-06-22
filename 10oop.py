17376fb1cf13beea9b90058416381881748359e7
Ayah Elsheikh <ayahhesham0@gmail.com>
2022-06-23 00:08:28 +0200

    first file in oop

---
+++ 10oop.py
@@ -0,0 +1,14 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+class calculator:
+    def sum(self,x,y):
+        print(x+y)
+    def mull(self,x,y):
+        print(x*y)
+
+        
+s1=calculator()
+s1.sum(7,8)
+15
+s1.mull(6,8)
+48
