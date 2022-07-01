5f1171f5e12eccc5ad6360ac710d2bae7da89488
Ayah Elsheikh <ayahhesham0@gmail.com>
2022-07-02 00:52:31 +0200

    editing in files

--- 12oop.py
+++ 12oop.py
@@ -1,5 +1,3 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 class A:
     def do(self):
         print('in A')
@@ -15,18 +13,9 @@ class c:
         
 class D(c,B):
     pass
-
-01=d()
-SyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers
-1=D()
-SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
-l=D()
-1.do()
-SyntaxError: invalid decimal literal
-l.do()
-in c
 class f(B,c):
     pass
+
 
 h=f()
 h.do()
